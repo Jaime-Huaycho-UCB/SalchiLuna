78b49903809f90019932d4645f47fbc7aa0c32f8
Jimmy-Macbook <huaychojaime@gmail.com>
2024-10-26 00:41:50 -0400

    configuracion de main

--- src/main/java/SalchiLuna/controller/Aplicacion.java
+++ src/main/java/SalchiLuna/controller/Aplicacion.java
@@ -1,7 +1,9 @@
 package SalchiLuna.controller;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println("Spring boot");
     }
 }
